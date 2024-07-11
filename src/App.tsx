5eb95094402018c3165231dce051ab4f4d88b0c8
Rodolphe <dutchr@live.fr>
2024-07-11 12:18:54 +0200

    Adding input AddNewItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { AddNewItem } from "./components/AddNewItem";
+import Card from "./components/Card";
 import { Column } from "./components/Column";
 import { AppContainer } from "./styles";
 
@@ -5,9 +7,20 @@ function App() {
   return (
     <>
       <AppContainer>
-        <Column text="A faire" />
-        <Column text="En cours" />
-        <Column text="Fini" />
+        <Column text="A faire">
+          <Card text="Apprendre le code" />
+        </Column>
+        <Column text="En cours">
+          <Card text="Apprendre le javascript" />
+        </Column>
+        <Column text="Terminé">
+          <Card text="Apprendre le HTML" />
+        </Column>
+
+        <AddNewItem
+          toggleButtonText=" + Ajouter une autre liste"
+          onAdd={console.log}
+        />
       </AppContainer>
     </>
   );
--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,20 +1,21 @@
-import { FC } from "react";
+import React,{ FC } from "react";
 import { ColumnContainer, ColumnTitle } from "../styles";
 import Card from "./Card";
+import { AddNewItem } from "./AddNewItem";
 
 type ColumnProps = {
   text: string;
+  children?: React.ReactNode
 };
 
-export const Column: FC<ColumnProps> = ({ text }) => {
+export const Column: FC<ColumnProps> = ({ text, children }) => {
   return (
     <ColumnContainer>
       <ColumnTitle>
         <div>{text}</div>
       </ColumnTitle>
-      {/* {children} */}
-      <Card text="je suis là" />
-      <Card text="Learn Typescript" />
+      {children}
+      <AddNewItem toggleButtonText=" + Ajouter une autre tâche" onAdd={console.log} dark/>
     </ColumnContainer>
   );
 };
